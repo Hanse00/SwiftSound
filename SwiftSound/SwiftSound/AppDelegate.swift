d004813f523f49f8ee6ba693457b2228c6abe3cc
Philip Hansen <me@philiphansen.dk>
2015-11-03 16:40:35 +0100

    Base Xcode Project
    Created a project in Xcode for an OS X application.

---
+++ SwiftSound/SwiftSound/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftSound
+//
+//  Created by Philip Hansen on 03/11/15.
+//  Copyright © 2015 Philip Hansen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftSound/SwiftSound/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftSound
+//
+//  Created by Philip Hansen on 03/11/15.
+//  Copyright © 2015 Philip Hansen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
